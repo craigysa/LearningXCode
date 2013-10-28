f50899049067a9207a9a1b9e0d8b365e03089b64
Craig Young <craigyoung@Craigs-Mac-mini.local>
2013-10-28 22:55:03 +0200

    Adding some tests to check understanding of ARC behaviour.

--- HelloWorld/HelloWorldTests/HelloWorldTests.m
+++ HelloWorld/HelloWorldTests/HelloWorldTests.m
@@ -12,6 +12,12 @@
 
 @end
 
+@interface ARCTests : XCTestCase
+@property BOOL isObjectDestroyed;
+@property (strong, nonatomic) NSObject *strongReference;
+@property (weak, nonatomic) NSObject *weakReference;
+@end
+
 @implementation HelloWorldTests
 
 - (void)setUp
@@ -28,7 +34,80 @@
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    NSString *s = [NSString new];
+    XCTAssertNotNil(s, @"Bad news");
 }
 
 @end
+
+@interface SimpleObject : NSObject
+@property (weak, nonatomic) ARCTests *testCase;
+@property (strong, nonatomic) NSObject *strongReference;
+@end
+
+
+@implementation ARCTests
+
+- (void)testObjectDestroyed
+{
+    self.isObjectDestroyed = NO;
+    SimpleObject *object = [SimpleObject new];
+    XCTAssertNotNil(object, @"Object is assigned");
+    object.testCase = self;
+    object = nil;
+    XCTAssertTrue(self.isObjectDestroyed, @"Object has been destroyed");
+}
+
+-(void)testObjectWithStrongReferenceNotDestroyed
+{
+    self.isObjectDestroyed = NO;
+    SimpleObject *object = [SimpleObject new];
+    object.testCase = self;
+    self.strongReference = object;
+    object = nil;
+    XCTAssertFalse(self.isObjectDestroyed, @"Object cannot be destroyed while strong reference exists.");
+    self.strongReference = nil;
+    XCTAssertTrue(self.isObjectDestroyed, @"Object has been destroyed");
+}
+
+-(void)testObjectWithWeakReferenceDestroyed
+{
+    self.isObjectDestroyed = NO;
+    SimpleObject *object = [SimpleObject new];
+    object.testCase = self;
+    self.weakReference = object;
+    object = nil;
+    XCTAssertTrue(self.isObjectDestroyed, @"Object has been destroyed");
+    XCTAssertNil(self.weakReference, @"Weak reference set to nil");
+}
+
+-(void)testCircularReferenceLeaksMemory
+{
+    self.isObjectDestroyed = NO;
+    SimpleObject *object1 = [SimpleObject new];
+    SimpleObject *object2 = [SimpleObject new];
+    /* Either instance can be used to mark that an object has been destroyed. */
+    object1.testCase = self;
+    object2.testCase = self;
+    /* Set up the circular reference. */
+    object1.strongReference = object2;
+    object2.strongReference = object1;
+    /* Clear the local references */
+    object1 = nil;
+    object2 = nil;
+
+    XCTAssertFalse(self.isObjectDestroyed, @"Neither object destroyed while they are looking at each other.");
+}
+
+@end
+
+@implementation SimpleObject
+- (void)dealloc
+{
+    if (self.testCase != nil) {
+        self.testCase.isObjectDestroyed = YES;
+    }
+}
+@end
+
